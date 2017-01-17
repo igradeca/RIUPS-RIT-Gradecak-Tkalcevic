e8412841a05f41cc8eedd5b79a5554712055e6cc
blackadder45 <luka.tkalcevic@hotmail.com>
2017-01-17 16:03:46 +0100

    Shop item klasa

---
+++ RIUPS Stack/Assets/Scripts/streznik/ShopItem.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace StackClone
+{
+    class ShopItem
+    {
+        public ShopItem() { }
+
+        public ShopItem( int id, string naziv )
+        {
+            Id = id;
+            Naziv = naziv;
+        }
+
+        public int Id { get; set; } = -1;
+
+        public string Naziv { get; set; } = "";
+
+
+        /// <summary>
+        /// Dodavanje novih ship itema
+        /// </summary>
+        /// <param name="shop item">Objekt s shop itemom kateroga hočemo dodati v bazo</param>
+        /// <returns></returns>
+        public static bool Dodaj( ShopItem item )
+        {
+            SqlConnection con = new SqlConnection( Nastavitve.GetConnectionString() );
+            SqlCommand cmd = new SqlCommand();
+
+            string into = "[StackDB].[dbo].[tblShop]";
+            string insert = "INSERT INTO " + into + " (Naziv) VALUES ('" + item.Naziv + "');";
+
+            cmd.CommandText = insert;
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch ( TimeoutException tEx )
+            {
+                // Zapisivanje u log
+                return false;
+            }
+            catch ( Exception ex )
+            {
+                // log
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Dodavanje novog shop itema za uporabnika
+        /// </summary>
+        public static bool DodajShopItemZaUporabnika( int idUporabnika, int idShopItema )
+        {
+            SqlConnection con = new SqlConnection( Nastavitve.GetConnectionString() );
+            SqlCommand cmd = new SqlCommand();
+
+            string into = "[StackDB].[dbo].[tblKupljeniArtikli]";
+            string insert = "INSERT INTO " + into + " (IdUporabnika, IdDosezka) VALUES (" +
+                idUporabnika + ", " + idShopItema + ");";
+
+            cmd.CommandText = insert;
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch ( TimeoutException tEx )
+            {
+                // Zapisivanje u log
+                return false;
+            }
+            catch ( Exception ex )
+            {
+                // log
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Seznam shop itema katere je uporabnik odljučal
+        /// </summary>
+        /// <returns>seznam shop itema</returns>
+        public static List<ShopItem> Brskaj()
+        {
+            SqlConnection con = new SqlConnection( Nastavitve.GetConnectionString() );
+            SqlCommand cmd = new SqlCommand();
+
+            string from = "[StackDB].[dbo].[tblShop]";
+            //                       0    1
+            string select = "SELECT Id, Naziv FROM " + from;
+
+            cmd.CommandText = select;
+            cmd.Connection = con;
+
+            List<ShopItem> lista = new List<ShopItem>();
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while ( reader.Read() )
+                {
+                    int idItema = reader.GetInt32( 1 );
+                    string naziv = reader.GetString( 2 );
+
+                    lista.Add( new ShopItem(idItema, naziv ));
+                }
+                reader.Close();
+                return lista;
+            }
+            catch ( TimeoutException tEx )
+            {
+                // Zapisivanje u log
+                return null;
+            }
+            catch ( Exception ex )
+            {
+                // log
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+
+        /// <summary>
+        /// Seznam shop itema katere je uporabnik odljučal
+        /// </summary>
+        /// <returns>seznam shop itema</returns>
+        public static List<int> BrskajZaUporabnika( int idUporabnika )
+        {
+            SqlConnection con = new SqlConnection( Nastavitve.GetConnectionString() );
+            SqlCommand cmd = new SqlCommand();
+
+            string where = "1 = 1 ";
+            where += (idUporabnika != -1) ? ("AND (IdUporabnika = " + idUporabnika + ") ") : ("");
+
+            string from = "[StackDB].[dbo].[tblKupljeniArtikli]";
+            //                           0             1
+            string select = "SELECT IdUporabnika, IdItema FROM " + from + " WHERE " + where;
+
+
+            cmd.CommandText = select;
+            cmd.Connection = con;
+
+            List<int> lista = new List<int>();
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while ( reader.Read() )
+                {
+                    int idItema = reader.GetInt32( 1 );
+
+                    lista.Add( idItema );
+                }
+                reader.Close();
+                return lista;
+            }
+            catch ( TimeoutException tEx )
+            {
+                // Zapisivanje u log
+                return null;
+            }
+            catch ( Exception ex )
+            {
+                // log
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
